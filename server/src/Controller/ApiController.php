ae6782559a14bcc4d457aecdfaf4c8ff49472ea6
Tene <tene.coulibaly@epitech.eu>
2022-07-12 17:19:01 +0200

    Ajout des groupe sur id

--- server/src/Controller/ApiController.php
+++ server/src/Controller/ApiController.php
@@ -16,7 +16,7 @@ class ApiController extends AbstractController
         public function getArticles(ArticleRepository $articleRepository, NormalizerInterface $normalize, SerializerInterface $serializerInterface)
         {
 
-            // $article = $articleRepository->findAll();                                               //RECUPERATION DANS LA BDD
+            // $article = $articleRepository->findAll();                                                  //RECUPERATION DANS LA BDD
 
             // $articleRepository = $normalize->normalize($article, null, ['groups' => 'groupe:get']);    //CONVERTIT EN TABLEAU ASSOCIATIF
             // $json = json_encode($articleRepository);                                                   //L'ENCODE EN JSON
@@ -36,4 +36,6 @@ class ApiController extends AbstractController
             return $this->json($articleRepository->findAll(), 200,[],['groups' => 'groupe:get']);
             //
         }
+
+        
 }
