7d290ed4e58f543e4639f9b53c5a41e002e1a86a
VinyCm <viny.come@epitech.eu>
2022-07-20 16:57:24 +0200

    Filtre OK & All reset

--- client/src/pages/categories/Categorie.jsx
+++ client/src/pages/categories/Categorie.jsx
@@ -1,75 +1,3 @@
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import axios from 'axios';
-// import Card from '../../components/card/Card';
-
-// const CategorieReact = () => {
-//   const [data, setData] = useState([]);
-//   const [dataArticles, setdataArticles] = useState([]);
-//   const [resultCat, setresultCat] = useState([]);
-
-//   useEffect(() => {
-//       const callAPI = async () => {
-//         axios.get('/api/categorie')
-//           .then(res => {
-//             setData(res.data);
-//           })
-//           .catch(err => {
-//             console.log(err);
-//           });
-//       }
-//       callAPI();
-//     }, []);
-
-//     useEffect(() => {
-//       const callAPI = async () => {
-//         axios.get('/api/articles')
-//           .then(res => {
-//               setdataArticles(res.data);
-//           })
-//           .catch(err => {
-//             console.log(err);
-//           });
-//       }
-//       callAPI();
-//     }, []);
-
-//     const filter = () => {
-//       const filterResult = dataArticles.filter((curData) =>{
-//           return curData.category === resultCat;
-//       });
-//       setdataArticles(filterResult);
-//       console.log(filterResult, "filterdata");
-//     }
-
-//     // console.log(dataArticles, 'res')
-
-//     return (
-//       <>
-//       <div className='homeContainer'>
-//           <fieldset>
-//           <legend>Choose your categorie's:</legend>
-//           <div>
-//                   {data.map((value) => (
-//                   <div>
-//                   <button type="button" id={value} name={value} onChange={() => setresultCat(value.titre)}>{value.titre}</button>
-//                   </div>
-//               ))}
-//               <button type="button" onClick={filter}>Submit</button>
-//           </div>
-//           </fieldset>
-//       </div>
-
-//       <div className='homeContainer'>
-//           {dataArticles.map((value) => (
-//           <Card imgSrc={value.photo} title={value.titre} price={value.prix + "€"} characteristic={value.caracteristique} key={value.id}/>
-//           ))}
-//       </div>
-//       </>
-//     )
-// }
-
-// export default CategorieReact
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from 'axios';
@@ -114,7 +42,7 @@ const CategorieReact = () => {
       });
       setdataArticles(filterResult);
       Reset();
-      console.log(filterResult, "filterdata");
+      // console.log(filterResult, "filterdata");
     }
 
     const Reset = () =>{
@@ -122,10 +50,8 @@ const CategorieReact = () => {
         setdataArticles(ResetdataArticles);
       }
     }
-
-    console.log(dataArticles, 'res')
-    console.log(ResetdataArticles, 'resetData')
-
+    // console.log(resultCat);
+    
     return (
       <>
       <div className='homeContainer'>
@@ -137,6 +63,7 @@ const CategorieReact = () => {
                   <button type="button" id={value} name={value} onClick={() => setresultCat(value.titre)}>{value.titre}</button>
                   </div>
               ))}
+              <button type="button" onClick={Reset}>All</button>
               <button type="button" onClick={filter}>Submit</button>
           </div>
           </fieldset>
