83521ced596cfa852affa37f94f4dac98c357a60
Tene <tene.coulibaly@epitech.eu>
2022-07-21 17:12:46 +0200

    recuperation du pull de majid

--- server/src/Controller/LikeController.php
+++ server/src/Controller/LikeController.php
@@ -4,19 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Like;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LikeController extends AbstractController
 {
-    #[Route('/like/{id}', name: 'app_like')]
-    public function Like($id, EntityManagerInterface $em)
+    #[Route('/like', name: 'app_like')]
+    public function Like()
     {
-        $new = $em->getRepository(Like::class)->find($id);
-     
-
-        dd($new);
+        // $new = $em->getRepository(User::class)->find($id);
+        
+        
+        // dd($new);
+        return $this->json($this->getUser(), 200,[],['groups' => 'groupe:get']);
 
     }
 }
