94c7b4990ee98db65f31430fcd22b0733cca2823
Majid Eltayeb <majideltayeb@MacBook-Pro-de-Majid.local>
2022-07-22 17:24:14 +0200

    i

--- client/model/InputModel.js
+++
@@ -1,8 +0,0 @@
-export function InputModel(type,change,value = "",placeholder = ""){
-    this.type = type;
-    this.placeholder = placeholder;
-    this.value = value;
-    this.change = change;
-}
-
-// export default InputModel;/
