e04f53efd2ddacf54a99521e195e70c3f1b91997
VinyCm <viny.come@epitech.eu>
2022-07-22 14:40:32 +0200

    Re-init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import Landing from "./pages/landing/Landing";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
-import Categorie from "./pages/categories/Categorie";
  
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Route path="/home" element={<Home/>}/>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/categorie" element={<Categorie/>}/>
       <Route path="*" element={<Landing/>}/>
     </Routes>
   );
