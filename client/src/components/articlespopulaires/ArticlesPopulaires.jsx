1fbb0ec649e86822b805627504b100456afbfab5
christophef <christophe.feuillard78320@gmail.com>
2022-07-27 12:47:20 +0200

    ADD component ArticlesPopulaires

---
+++ client/src/components/articlespopulaires/ArticlesPopulaires.jsx
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react';
+import './ArticlesPopulaires.css';
+import {useNavigate} from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import axios from 'axios';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const Card = () => {
+
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const callAPI = () => {
+      axios.get('http://127.0.0.1:8000/api/articles')
+        .then(res => {
+          setData(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    callAPI();
+
+  }, []);
+
+  const sorted = data.map(d => ({id: d.id, titre: d.titre, prix: d.prix, photo: d.photo, description: d.description, visit: d.visit, caracteristique: d.caracteristique})).sort((el1, el2) => el2.visit - el1.visit)
+  console.log(data)
+  
+  return (
+  <div className='div_articlepopulaires'>
+    <h2>Articles les plus populaires</h2>
+    <Swiper
+    modules={[Navigation, Pagination, Scrollbar, A11y]}
+    spaceBetween={50}
+    slidesPerView={2}
+    navigation
+    pagination={{ clickable: true }}
+    scrollbar={{ draggable: true }}
+    onSwiper={(swiper) => console.log(swiper)}
+    onSlideChange={() => console.log('slide change')}
+    >
+    {sorted.map((item,key) => ( key < 5 &&
+      <SwiperSlide> 
+        <div onClick={() => navigate("/article_details/"+item.id)} className='cardCarou'>
+          <img src={item.photo} alt="image du produit"/>
+          <div>
+            <span className="spanCarou">{item.titre}</span>  
+          </div>
+        </div>
+      </SwiperSlide>
+    ))}
+    ...
+    </Swiper>
+  </div>
+
+  )
+}
+
+export default Card
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,17 +2,15 @@ import React, {useEffect,useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import Header from "../../components/header/Header";
 import Card from '../../components/card/Card';
+import ArticlesPopulaires from "../../components/articlespopulaires/ArticlesPopulaires";
 import axios from 'axios';
 import "./Home.css";
 // import NavItems from '../../components/navItem/NavItems';
 
 const Home = () => {
-  
   const navigate = useNavigate();
-
   const [data, setData] = useState([]);
   const [search,setSearch] = useState("");
-  const [order,setOrder] = useState("ASC");
   
   useEffect(() => {
     const callAPI = () => {
@@ -28,43 +26,13 @@ const Home = () => {
 
   }, []);
 
-  // const datatest = [{
-  //   "id": 0,
-  //   "name": "Vernon Dunham",
-  //   "company": "Qualcore",
-  //   "email": "vernon.dunham@qualcore.com"
-  // }, {
-  //   "id": 1,
-  //   "name": "Dori Neal",
-  //   "company": "Sunopia",
-  //   "email": "dori.neal@sunopia.com"
-  // }, {
-  //   "id": 2,
-  //   "name": "Rico Muldoon",
-  //   "company": "Airconix",
-  //   "email": "rico.muldoon@airconix.com"
-  // }]
-  
-  // const sorted = datatest.map(d => ({id: d.id, name: d.name})).sort((el1, el2) => el2.id - el1.id)
-  // console.log(sorted)
-
-  const sorted = data.map(d => ({id: d.id, titre: d.titre, prix: d.prix, photo: d.photo, description: d.description, visit: d.visit, caracteristique: d.caracteristique})).sort((el1, el2) => el2.visit - el1.visit)
-  console.log(data)
-
 return (
   <>
     <Header search={search} change={(e)=>setSearch(e.target.value)}/>
+
     <div className='homeContainer'>
 
-      <h2>Articles les plus populaires</h2>
-      <div className='div_articlepopulaire'>
-      {sorted.map((item,key) => (
-        key < 3 && <Card imgSrc={item.photo} title={item.titre} price={item.prix + "€"} characteristic={item.caracteristique}
-          handleckick={()=> navigate("/article_details/"+item.id)} 
-          // handleckick={()=> countItem(item.id)}
-        />
-      ))}
-      </div>
+     <ArticlesPopulaires/>    
 
       <div className='div_toutlesarticles'>
       {data.map((item,key) => (
@@ -74,8 +42,6 @@ return (
         />
       ))}
       </div>
-
-
 
     </div>
   </>
